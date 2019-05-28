e9ddd310d2bf25f0e0a5e68589bff98064d02d1a
Marc Debold <debold@users.noreply.github.com>
2019-05-28 08:41:23 +0200

    Initial release

---
+++ Start-ExchangeMaintenanceMode.ps1
@@ -0,0 +1,107 @@
+﻿<#
+.SYNOPSIS
+    Places the given Exchange server into maintenance mode.
+.DESCRIPTION
+    This script uses the procedure described by Paul Cunningham on 
+    https://practical365.com/exchange-server/installing-cumulative-updates-on-exchange-server-2016/
+    to prepare a Exchange server for maintenance (e.g. installing updates).
+    The server provided SHOULD be part of a DAG, so that automatic distribution of databases can be done.
+.LINK
+    http://bechtle.com
+.NOTES
+    Written by: Marc Debold (marc.debold@bechtle.com)
+
+    Change Log 
+    V1.00, 27.04.2017 - Initial version 
+.OUTPUTS
+    Shows status information about the progress of starting the maintenance mode.
+.PARAMETER Identity
+    Used for the name of the Exchange server, that should be put into maintenance mode.
+.PARAMETER PartnerServer
+    The optional PartnerServer defines the target for moving unprocessed messages in the server's transport queues to.
+.EXAMPLE
+    Start-ExchangeMaintenanceMode -Identity ExchSrv01
+    Puts Exchange Server ExchSrv01 into maintenance mode without moving the transport queues.
+.EXAMPLE
+    Start-ExchangeMaintenanceMode -Identity ExchSrv01 -PartnerServer ExchSrv02
+    Puts Exchange Server ExchSrv01 into maintenance mode while moving the transport queues to server ExchSrv02.    
+#>
+
+[CmdletBinding()] param(
+    [Parameter(Mandatory=$true,Position=0)]$Identity,
+    [Parameter(Mandatory=$false,Position=1)]$PartnerServer
+)
+
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
+$isError = $false
+try {
+    Write-Host "Checking source server $($Identity) ... " -NoNewline
+    $SourceServer = Get-ExchangeServer -Identity $Identity -ErrorAction Stop
+    Write-Host "okay." -ForegroundColor Green
+    if ($PartnerServer -ne $null) {
+        Write-Host "Checking target server $($PartnerServer) ... " -NoNewline
+        $TargetServer = Get-ExchangeServer -Identity $PartnerServer -ErrorAction Stop
+        Write-Host "okay." -ForegroundColor Green
+    }
+} catch {
+    Write-Host "failed." -ForegroundColor Red
+    $isError = $true
+}
+
+if (-not $isError) {
+
+    # Hub Transport auf Draining stellen
+    Write-Host "Draining hub transport role ... " -NoNewline
+    Set-ServerComponentState -Identity $SourceServer.Name –Component HubTransport –State Draining –Requester Maintenance
+    Write-Host "done." -ForegroundColor Green
+    
+    if ($PartnerServer -ne $null) {
+        # Nachrichten auf anderen Server umleiten
+        Write-Host "Redirecting message queues from $($SourceServer.Name) to $($TargetServer.Name) ... " -NoNewline
+        Redirect-Message -Server $SourceServer.Name -Target $TargetServer.Fqdn -Confirm:$false
+        Write-Host "done." -ForegroundColor Green
+    }
+
+    # Clusterknoten im DAG deaktivieren
+    Write-Host "Suspending cluster node ... " -NoNewline
+    Suspend-ClusterNode –Name $SourceServer.Name | Out-Null
+    Write-Host "done." -ForegroundColor Green
+
+    # Autoaktivierung DB abschalten
+    Write-Host "Disabling database copy actovation ... " -NoNewline
+    Set-MailboxServer -Identity $SourceServer.Name –DatabaseCopyActivationDisabledAndMoveNow $true
+    Write-Host "done." -ForegroundColor Green
+
+    # AutoActivationPolicy notieren, dann blockieren
+    $ActivationPolicy = (Get-MailboxServer -Identity $SourceServer.Name).DatabaseCopyAutoActivationPolicy
+    Write-Host "Setting activation policy to BLOCKED (was: $($ActivationPolicy)) ... " -NoNewline
+    Set-MailboxServer -Identity $SourceServer.Name –DatabaseCopyAutoActivationPolicy Blocked
+    Write-Host "done." -ForegroundColor Green
+
+    $FirstLoop = $true
+    # Prüfen, ob noch Datenbanken auf dem Server laufen --> sollte keine mehr sein
+    Write-Host "Checking for active databases ... " -NoNewline
+    do {
+        if (-not $FirstLoop) {
+            Write-Host "Waiting some seconds for a retry ... " -NoNewline
+            Start-Sleep -Seconds 10
+        }
+        $MountedDb = Get-MailboxDatabaseCopyStatus -Server $SourceServer.Name | Where {$_.Status -eq "Mounted"}
+        $MountedDbCount = $MountedDb.Count
+        $FirstLoop = $false
+        if ($MountedDbCount -gt 0) {
+            Write-Host "$($MountedDbCount) found: $($MountedDb.Name -join ", ")." -ForegroundColor Red
+        } else {
+            Write-Host "none found." -ForegroundColor Green
+        }
+    } while ($MountedDbCount -gt 0)
+
+    # Server in den Wartungsmodus setzen
+    Write-Host "Setting server to maintenacne mode ... " -NoNewline
+    Set-ServerComponentState -Identity $SourceServer.Name –Component ServerWideOffline –State InActive –Requester Maintenance
+    Write-Host "done." -ForegroundColor Green
+    Write-Host "`n########################################`n# Server is now ready for maintenance. #`n########################################`n"
+    Write-Host "Please remember the former DatabaseCopyAutoActivationPolicy: " -NoNewline
+    Write-Host "$($ActivationPolicy)" -ForegroundColor Green
+}
---
+++ Stop-ExchangeMaintenanceMode.ps1
@@ -0,0 +1,100 @@
+﻿<#
+.SYNOPSIS
+    Resumes the given Exchange server from maintenance mode.
+.DESCRIPTION
+    This script uses the procedure described by Paul Cunningham on 
+    https://practical365.com/exchange-server/installing-cumulative-updates-on-exchange-server-2016/
+    to resume services of a Exchange server after maintenance (e.g. installing updates).
+    The server provided SHOULD be part of a DAG, so that automatic distribution of databases can be done.
+.LINK
+    http://bechtle.com
+.NOTES
+    Written by: Marc Debold (marc.debold@bechtle.com)
+
+    Change Log 
+    V1.00, 27.04.2017 - Initial version 
+.OUTPUTS
+    Shows status information about the progress of ending the maintenance mode.
+.PARAMETER Identity
+    Used for the name of the Exchange server, that should be resumed from maintenance mode.
+.PARAMETER DatabaseCopyAutoActivationPolicy
+    When entering maintenance mode, the ActivationPolicy is set to BLOCKED. To resume after maintenance, the ActivationPolicy
+    needs to be restored to the same value, as before. If the corresponding script was used, the ActivationPolicy was printed
+    out while starting the maintenance mode. This parameter can be used to restore this setting. If omitted, UNRESTRICTED is
+    used by default
+.EXAMPLE
+    Stop-ExchangeMaintenanceMode -Identity ExchSrv01
+    Resumes operation for Exchange Server ExchSrv01.
+.EXAMPLE
+    Stop-ExchangeMaintenanceMode -Identity ExchSrv01 -DatabaseCopyAutoActivationPolicy IntraSiteOnly
+    Resumes operation for Exchange Server ExchSrv01 setting the ActivationPolicy to INTRASITEONLY.
+#>
+
+[CmdletBinding()] param(
+    [Parameter(Mandatory=$true,Position=0)]$Identity,
+    [Parameter(Mandatory=$false,Position=1)][ValidateSet("Blocked", "IntrasiteOnly", "Unrestricted")]$DatabaseCopyAutoActivationPolicy = "Unrestricted"
+)
+
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
+$isError = $false
+try {
+    Write-Host "Checking server $($Identity) ... " -NoNewline
+    $SourceServer = Get-ExchangeServer -Identity $Identity -ErrorAction Stop
+    Write-Host "okay." -ForegroundColor Green
+} catch {
+    Write-Host "failed. Please enter a correct Exchange server name." -ForegroundColor Red
+    $isError = $true
+}
+
+if (-not $isError) {
+    # DAG Name herausfinden
+    Write-Host "Searching for DAG membership ... " -NoNewline
+    $DagName = (Get-DatabaseAvailabilityGroup | ? {$_.Servers -like $SourceServer.Name}).Name
+    if ($DagName -ne $null) {
+        Write-Host "found '$($DagName)'." -ForegroundColor Green
+    } else {
+        Write-Host "none found." -ForegroundColor Red
+    }
+    # Wartungsmodus aufheben
+    Write-Host "Disabling maintenacne mode ... " -NoNewline
+    Set-ServerComponentState $SourceServer.Name –Component ServerWideOffline –State Active –Requester Maintenance
+    Write-Host "done." -ForegroundColor Green
+
+    # Im DAG wieder aktivieren
+    Write-Host "Resuming cluster node ... " -NoNewline
+    $ClusterState = (Get-ClusterNode -Name $SourceServer.Name).State
+    if ($ClusterState -eq "Paused") {
+        try {
+            Resume-ClusterNode –Name $SourceServer.Name | Out-Null
+            Write-Host "done." -ForegroundColor Green
+        } catch {
+            Write-Host "error resuming cluster node." -ForegroundColor Red
+        }
+    } else {
+        Write-Host "cluster in state '$($ClusterState)'. Cannot resume." -ForegroundColor Red
+    }
+
+    # Autoaktivierung wieder reaktivieren
+    Write-Host "Setting activation policy to '$($DatabaseCopyAutoActivationPolicy)' ... " -NoNewline
+    Set-MailboxServer $SourceServer.Name –DatabaseCopyAutoActivationPolicy $DatabaseCopyAutoActivationPolicy
+    Write-Host "done." -ForegroundColor Green
+
+    # Autoaktivierung wieder einschalten
+    Write-Host "Enabling database copy actovation ... " -NoNewline
+    Set-MailboxServer $SourceServer.Name –DatabaseCopyActivationDisabledAndMoveNow $false
+    Write-Host "done." -ForegroundColor Green
+
+    # Hub Transport wieder aktivieren
+    Write-Host "Resuming hub transport role ... " -NoNewline
+    Set-ServerComponentState $SourceServer.Name –Component HubTransport –State Active –Requester Maintenance
+    Write-Host "done." -ForegroundColor Green
+
+    if ($DagName -ne $null) {
+        # Datenbanken wieder neu verteilen
+        Write-Host "Rebalancing databases for DAG '$($DagName)' ... " -NoNewline
+        & "$($env:ExchangeInstallPath)\Scripts\RedistributeActiveDatabases.ps1" -DagName $DagName -BalanceDbsByActivationPreference -Confirm:$false
+        Write-Host "done." -ForegroundColor Green
+    }
+    Write-Host "`n#####################################`n# Server is now out of maintenance. #`n#####################################`n"
+}
